578b537e14170d6c2b1c4a4180625d3d6dfd493a
Pro-CG <105145965+Pro-CG@users.noreply.github.com>
2022-05-11 11:02:37 +0100

    config.py
    git clone https://github.com/SpEcHiDe/PublicLeech.git
    cd PublicLeech
    virtualenv -p /usr/bin/python3 venv
    . ./venv/bin/activate
    pip install -r requirements.txt
    # <Create config.py appropriately>
    python3 -m tobrot

---
+++ config.py
@@ -0,0 +1,13 @@
+git clone https://github.com/SpEcHiDe/PublicLeech.git
+
+cd PublicLeech
+
+virtualenv -p /usr/bin/python3 venv
+
+. ./venv/bin/activer
+
+pip install -r exigences.txt
+
+# <Créer config.py de manière appropriée>
+
+python3 -m tobrot
